f0f9b2adf6116be698a9f0da008be5962d03c195
vk <vk@analytic-systema.de>
2021-02-12 18:34:33 +0100

    Create Project

---
+++ TestConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Security;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var from = new MailAddress("koenig.vitalij@web.de", "Vitalij");
+            var to = new MailAddress("koenig.vitalij@web.de", "Vitalij");
+
+            var message = new MailMessage(from, to);
+            message.Subject = "subject";
+            message.Body = "body";
+            var client = new SmtpClient("smtp.gmx.de", 465);
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "***",
+                //SecurePassword = ""
+                Password = ""
+            };
+            client.Send(message);
+        }
+    }
+}
---
+++ TestWPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
+
+namespace TestWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var from = new MailAddress("koenig.vitalij@web.de", "Vitalij");
+            var to = new MailAddress("koenig.vitalij@web.de", "Vitalij");
+
+            var message = new MailMessage(from, to);
+            message.Subject = "subject";
+            message.Body = "body";
+            var client = new SmtpClient("smtp.web.de", 25)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential {UserName = LoginName.Text, Password = Password.Password}
+            };
+
+            try
+            {
+                client.Send(message);
+                MessageBox.Show("Alles korrekt angekommen", "Information", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            }
+            catch (SmtpException exception)
+            {
+                MessageBox.Show("Smtp Problem","Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (TimeoutException exception)
+            {
+                MessageBox.Show("TimeOut", "Error", MessageBoxButton.OK, MessageBoxImage.Stop);
+            }
+        }
+    }
+}
